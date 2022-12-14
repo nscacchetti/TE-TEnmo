cbc8a4e0b975ed51601ebaecc08950867812a4dc
Joe <joejr.hasson@gmail.com>
2022-12-14 12:02:22 -0500

    commit for updates with dB adds

--- database/tenmo.sql
+++ database/tenmo.sql
@@ -33,5 +33,21 @@ CREATE TABLE account (
 	CONSTRAINT FK_account_tenmo_user FOREIGN KEY (user_id) REFERENCES tenmo_user (user_id)
 );
 
+CREATE SEQUENCE seq_transfer_id
+  INCREMENT BY 1
+  START WITH 3001
+  NO MAXVALUE;
+
+CREATE TABLE transfers (
+transfer_id int NOT null DEFAULT nextval ('seq_transfer_id'),
+	from_user_id int NOT null,
+	to_user_id int NOT null,
+	transfer_amt numeric(13,2) NOT null,
+	CONSTRAINT CHK_users CHECK (to_user_id <> from_user_id),
+	CONSTRAINT PK_transfers PRIMARY KEY (transfer_id),
+	CONSTRAINT FK_transfers_from_user FOREIGN KEY (from_user_id) REFERENCES tenmo_user (user_id),
+	CONSTRAINT FK_transfers_to_user FOREIGN KEY (to_user_id) REFERENCES tenmo_user (user_id)
+);
+
 
 COMMIT;
