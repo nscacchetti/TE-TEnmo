421365c795012e5a11986a09199bff7aecd6e51d
Nicholas Scacchetti <nscacchetti@gmail.com>
2022-12-16 12:13:28 -0500

    added not for andy

--- server/src/test/java/AppControllerTests.java
+++ server/src/test/java/AppControllerTests.java
@@ -19,7 +19,7 @@ import java.util.List;
 import static org.junit.Assert.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AppControllerTests {
-
+//@TODO ask andy about forks
     private static final String API_BASE_URL = "http://localhost:8080";
     private final RestTemplate restTemplate = new RestTemplate();
     private TokenDTO user1Token;
